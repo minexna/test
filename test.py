d539b169e140dbeeed88d8d22b2cb40235d42fc3
Zacniewski Artur <tcelkat18@gmail.com>
2018-03-18 12:01:13 +0100

    all

---
+++ test.py
@@ -0,0 +1,14 @@
+print("Tekst")
+#to\worzenie funkcji
+
+def napis():
+    print('test funkcji')
+	
+napis()
+
+if 7>5:
+    print("7>5")
+    print("liczby drugie")
+	
+for i in range(0, 10):
+    print(i)
