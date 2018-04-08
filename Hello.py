55a15d383f795632bb12e03e6ecd5ea1757c92ba
Zacniewski Artur <tcelkat18@gmail.com>
2018-04-08 10:32:39 +0200

    przygotowanie do heroku

--- Hello.py
+++ Hello.py
@@ -5,5 +5,21 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 	
+@app.route("/test")
+def test():
+    return "test adresu"
+	
+@app.route("/suma")
+def suma():
+    a=5
+    b=6
+    return str(a+b)
+
+@app.route("/ilo")
+def ilo():
+    a=5
+    b=6
+    return str(a*b)
+	
 if __name__ == '__main__':
     app.run()
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn HELLO:app --log-file=-
