ba58264a2a1c2180730cb751e47d844fdf9547c1
Robert Klimek <rjk1903@poczta.onet.eu>
2018-04-22 10:28:04 +0200

    22kwietnia

--- Hello.py
+++ Hello.py
@@ -1,19 +1,25 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
+import socket
 
 @app.route("/")
 def hello():
-    return "Hello World!"
-	
+    #my=ip=get_ip()
+    return render_template("index.html", my_ip=get_ip(),moja_suma=22,moja_roznica=odejmowanie())
+def odejmowanie():
+    a=743
+    b=219
+    return str(a-b)
+
 @app.route("/test")
 def test():
     return "test adresu"
-	
-@app.route("/suma")
-def suma():
-    a=5
-    b=6
-    return str(a+b)
+    
+def get_ip():
+    host_name = socket.gethostname()
+    ip_address = socket.gethostbyname(host_name)
+    return ip_address
 
 @app.route("/ilo")
 def ilo():
